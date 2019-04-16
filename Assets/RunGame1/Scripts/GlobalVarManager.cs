afce7f314ef0f3d15f988db188db04143510de1b
Dien_Vincent <anhdien2210@gmail.com>
2019-04-16 21:01:30 +0700

    addcode

--- Assets/RunGame1/Scripts/GlobalVarManager.cs
+++ Assets/RunGame1/Scripts/GlobalVarManager.cs
@@ -141,6 +141,14 @@ public class GlobalVarManager : MonoBehaviour {
 	}
 	
 	void Update () {
-	
-	}
+        if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+        {
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+        }
+        else if (Input.deviceOrientation == DeviceOrientation.LandscapeRight)
+        {
+            Screen.orientation = ScreenOrientation.LandscapeRight;
+        }
+
+    }
 }
